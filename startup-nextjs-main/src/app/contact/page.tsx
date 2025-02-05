8282d3e84585cb537e432e995d2a606112baebfa
rigstire <brodsky.nick@gmail.com>
2025-02-04 22:15:51 -0800

    fixing contact

--- startup-nextjs-main/src/app/contact/page.tsx
+++ startup-nextjs-main/src/app/contact/page.tsx
@@ -3,23 +3,18 @@ import Contact from "@/components/Contact";
 
 import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: "Contact Page | Free Next.js Template for Startup and SaaS",
-//   description: "This is Contact Page for Startup Nextjs Template",
-//   // other metadata
-// };
+export const metadata: Metadata = {
+  title: "Contact Page | Free Next.js Template for Startup and SaaS",
+  description: "This is Contact Page for Startup Nextjs Template",
+  // other metadata
+};
 
 const ContactPage = () => {
   return (
     <>
       <Breadcrumb
-<<<<<<< HEAD
         pageName=""
         description=""
-=======
-        pageName="Contact Page"
-        description="Contact us now "
->>>>>>> d75a038b2f6427ffa4f69abe4adc951fc0c2cf2b
       />
 
       <Contact />
